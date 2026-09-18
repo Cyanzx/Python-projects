b9a94170589e766ab47f9040c2d54a5b3f3c7ceb
Cyanzx <102337307+Cyanzx@users.noreply.github.com>
2026-09-17 22:51:24 -0300

    Create respectivamente.py

---
+++ respectivamente.py
@@ -0,0 +1,8 @@
+
+number = int(input("Numero: "))
+    
+dobro = 2 * number
+triplo = 3 * number
+raiz = number ** (1/2)
+
+print(f" O dobro, o triplo e a raiz são respectivamente: {dobro,triplo,raiz}")
